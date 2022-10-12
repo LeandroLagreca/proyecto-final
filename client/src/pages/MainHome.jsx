c09b08a6be21fe55bb7542b5255f1cc4ae0843d4
megagus182 <megagus182@hotmail.com>
2022-10-12 12:59:40 -0500

    Fix Mainhome
    corrigo el dispacth

--- client/src/pages/MainHome.jsx
+++ client/src/pages/MainHome.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Home, About } from "../sections";
 import Paginated from "../components/Pagination/Pagination";
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getGames } from "../redux/actions/videoGame";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGames } from "../redux/actions/videoGame";
+import Cards from "../components/Cards/Cards";
 
 const MainHome = () => {
-  //   const games = useSelector((state) => state.games);
-  //   const dispatch = useDispatch();
-  //   useEffect(() => {
-  //     dispatch(getGames())
-  //   }, []);
+    const games = useSelector((state) => state.videogames.games);
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(getGames())
+    }, []);
 
   
   return (
     <div>
       <Home />
       <About />
+      <Cards games={games}/>
       <Paginated/>
     </div>
   );
