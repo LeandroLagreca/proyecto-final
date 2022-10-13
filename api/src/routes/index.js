d201d93129d149b9917a0d6611f2a0112d724df8
Federico Goldammer <federico.goldammer@gmail.com>
2022-10-13 17:22:20 -0300

    Controller de videogame, cambios en enrutado/model

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,30 @@
 const { Router } = require('express');
-const { videogameCodeoPost, videogameCodeoByID, allDataCodeoVideogames } = require('../controllers/hardcodeo.js');
+const { videogamePost, allDataVideogames, videogameByID, getGenres,updateVideogame } = require('../controllers/videogameControllers.js');
 const axios = require('axios');
+const { allDataUser, UserByID, UserPost, UserEliminated, UserUpdate } = require('../controllers/User.js');
 const router = Router();
 
 // Configuración de rutas
-router.get("/videogames/:id", videogameCodeoByID)
+router.get("/videogames/:id", videogameByID)
 
-router.post("/videogames", videogameCodeoPost)
+router.post("/videogames", videogamePost)
 
-router.get("/videogames", allDataCodeoVideogames)
+router.get("/videogames", allDataVideogames)
+
+router.get("/genres",getGenres)
+
+router.get("/user", allDataUser);
+
+router.post('/user', UserPost);
+
+router.get('/user/:id', UserByID);
+
+router.delete('/user/:id', UserEliminated);
+
+router.put('/user/:id', UserUpdate);
+
+router.put('/modify/:id',updateVideogame)
+
 
 
 module.exports = router;
