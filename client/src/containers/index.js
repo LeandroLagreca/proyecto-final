b054dea44b36a0c3eea44fed186a6836017a4dcc
Cristianbenz <contactocbenitez@gmail.com>
2022-10-13 20:00:06 -0300

    feat(client): add users section to admin panel

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -2,3 +2,4 @@ export {default as LandingContainer} from './LandingContainer';
 export {default as HomeContainer} from './HomeContainer';
 export {default as SectionContainer} from './SectionContainer';
 export {default as AdminContainer} from './AdminContainer';
+export {default as UsersContainer} from './UsersContainer';
