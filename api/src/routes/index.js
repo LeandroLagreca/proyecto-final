c286f18b96757fd654efbb5fb217c00b2d5d2dcb
leandro la greca <leandrolagreca@hotmail.com>
2022-10-13 17:40:25 -0300

    back fede

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,34 @@
 const { Router } = require('express');
-const { videogameCodeoPost, videogameCodeoByID, allDataCodeoVideogames,getGenres,updateVideogame } = require('../controllers/hardcodeo.js');
+const { videogamePost, allDataVideogames, videogameByID, getGenres,updateVideogame } = require('../controllers/videogameControllers.js');
 const axios = require('axios');
 const { allDataUser, UserByID, UserPost, UserEliminated, UserUpdate } = require('../controllers/User.js');
 const router = Router();
 
 // Configuración de rutas
-router.get("/videogames/:id", videogameCodeoByID)
+//Videogame
+router.get("/videogames/:id", videogameByID)
 
-router.post("/videogames", videogameCodeoPost)
+router.post("/videogames", videogamePost)
 
-router.get("/videogames", allDataCodeoVideogames)
+router.get("/videogames", allDataVideogames)
+
+router.put('/modify/:id',updateVideogame)
 
 router.get("/genres",getGenres)
+
+//User
+
 router.get("/user", allDataUser);
+
 router.post('/user', UserPost);
+
 router.get('/user/:id', UserByID);
+
 router.delete('/user/:id', UserEliminated);
+
 router.put('/user/:id', UserUpdate);
-router.put('/modify/:id',updateVideogame)
+
+
+
+
 module.exports = router;
