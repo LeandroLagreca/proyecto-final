2f3ef3fd56eace99992de7fdcc9c37d0f765618a
Cristianbenz <contactocbenitez@gmail.com>
2022-10-15 09:25:04 -0300

    fix: merge deleted features

--- client/src/sections/Games.jsx
+++ client/src/sections/Games.jsx
@@ -1,62 +1,71 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Box } from '@mui/material';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Box } from "@mui/material";
 
-import { changePage } from '../redux/reducers/videoGame';
+import { changePage } from "../redux/reducers/videoGame";
 
-import Card from '../components/Cards/Card';
-import Pagined from '../components/Pagination/Pagination';
+import Card from "../components/Cards/Card";
+import Pagined from "../components/Pagination/Pagination";
+
+const styles = {
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(210px, 1fr))",
+    justifyItems: "center",
+    paddingX: 10,
+  },
+};
 
 const Games = () => {
-	const allGames = useSelector((state) => state.videogames.filterGames);
-	const page = useSelector(state => state.videogames.page)
-	const [gamesPerPage, setGamesPerPage] = useState(5);
-	const indexOfLastGame = gamesPerPage * page; // 10
-	const indexOfFirtsGame = indexOfLastGame - gamesPerPage; // 0
-	const currentGames = allGames.slice(indexOfFirtsGame, indexOfLastGame);
-	const totalPages = Math.ceil(allGames.length / gamesPerPage)
+  const allGames = useSelector((state) => state.videogames.filterGames);
+  const page = useSelector((state) => state.videogames.page);
+  const [gamesPerPage, setGamesPerPage] = useState(5);
+  const indexOfLastGame = gamesPerPage * page; // 10
+  const indexOfFirtsGame = indexOfLastGame - gamesPerPage; // 0
+  const currentGames = allGames.slice(indexOfFirtsGame, indexOfLastGame);
+  const totalPages = Math.ceil(allGames.length / gamesPerPage);
 
-	const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-	return (
-		<div>
-			<Pagined 
-				setPage={(num) => dispatch(changePage(num))} 
-				setGamesPerPage={setGamesPerPage}
-				total={totalPages}
-				current={page}
-			/>
-			<Box display="flex" style={{justifyContent: "center"}} flexDirection={'row'}>
-				{currentGames?.map((e, index) => {
-					return (
-						<Box key={index}>
-							<ul
-								style={{
-									display: 'flex',
-									flexWrap: 'wrap',
-									width: "190px",
-									justifyContent: 'center',
-									listStyle: 'none',
-								}}
-							>
-								<li>
-									{
-										<Card
-											background_image={e.background_image}
-											name={e.name}
-											id={e.id}
-											price={e.price}
-											description={e.description}
-										/>
-									}
-								</li>
-							</ul>
-						</Box>
-					);
-				})}
-			</Box>
-		</div>
-	);
+  return (
+    <div>
+      <Pagined
+        setPage={(num) => dispatch(changePage(num))}
+        setGamesPerPage={setGamesPerPage}
+        total={totalPages}
+        current={page}
+      />
+      <Box sx={styles.container}>
+        {currentGames?.map((e, index) => {
+          return (
+            <Box key={index}>
+              <ul
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  width: "190px",
+                  justifyContent: "center",
+                  listStyle: "none",
+                }}
+              >
+                <li>
+                  {
+                    <Card
+                      background_image={e.background_image}
+                      name={e.name}
+                      id={e.id}
+                      price={e.price}
+                      description={e.description}
+                    />
+                  }
+                </li>
+              </ul>
+            </Box>
+          );
+        })}
+      </Box>
+    </div>
+  );
 };
 
 export default Games;
