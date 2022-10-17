10169c512675629f34abc50e967f5233bc5cad33
DAVID HUARICANCHA <davidjss.dev@gmail.com>
2022-10-17 11:25:11 -0500

    Se coloco como link el logo para que vaya al inicio y se oculto el perfil si esta como invitado

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,218 +1,234 @@
-import * as React from "react";
-import { Link } from 'react-router-dom'
-import { useSelector } from "react-redux";
+import * as React from 'react';
+import Link from '@mui/material/Link';
+import { useSelector } from 'react-redux';
 
 import {
-  AppBar,
-  Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Badge,
-  MenuItem,
-  Menu,
-  Button,
-  Divider
-} from "@mui/material";
+	AppBar,
+	Box,
+	Toolbar,
+	IconButton,
+	Typography,
+	Badge,
+	MenuItem,
+	Menu,
+	Button,
+	Divider,
+} from '@mui/material';
 
 import {
-  AccountCircle,
-  Mail as MailIcon,
-  Notifications as NotificationsIcon,
-  MoreVert as MoreIcon,
-} from "@mui/icons-material";
+	AccountCircle,
+	Mail as MailIcon,
+	Notifications as NotificationsIcon,
+	MoreVert as MoreIcon,
+} from '@mui/icons-material';
 
-import { CartWidget, SessionButton } from "../";
-import Searchbar from "./Searchbar";
+import { CartWidget, SessionButton } from '../';
+import Searchbar from './Searchbar';
 
 const styles = {
-  link: {
-    textDecoration: 'none'
-  }
-}
+	link: {
+		textDecoration: 'none',
+	},
+};
 
 const Navbar = () => {
-  const [input, setInput] = React.useState("");
-  const videogames = useSelector((state) => state.videogames.games);
-  const currentGames = videogames.filter((game) => {
-    return game.name.toLowerCase().includes(input.toLowerCase());
-  });
+	const user = useSelector((state) => state.user);
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+	const [input, setInput] = React.useState('');
+	const videogames = useSelector((state) => state.videogames.games);
+	const currentGames = videogames.filter((game) => {
+		return game.name.toLowerCase().includes(input.toLowerCase());
+	});
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const isMenuOpen = Boolean(anchorEl);
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
+	const handleProfileMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
+	const handleMobileMenuClose = () => {
+		setMobileMoreAnchorEl(null);
+	};
 
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+		handleMobileMenuClose();
+	};
 
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
+	const handleMobileMenuOpen = (event) => {
+		setMobileMoreAnchorEl(event.currentTarget);
+	};
 
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+	const menuId = 'primary-search-account-menu';
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			id={menuId}
+			keepMounted
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={isMenuOpen}
+			onClose={handleMenuClose}
+		>
+			<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+			<MenuItem onClick={handleMenuClose}>My account</MenuItem>
+		</Menu>
+	);
 
-  React.useEffect(() => {
-    console.log(currentGames);
-  }, [input]);
+	const mobileMenuId = 'primary-search-account-menu-mobile';
+	const renderMobileMenu = (
+		<Menu
+			anchorEl={mobileMoreAnchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			id={mobileMenuId}
+			keepMounted
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={isMobileMenuOpen}
+			onClose={handleMobileMenuClose}
+		>
+			<MenuItem>
+				<IconButton size="large" aria-label="show 4 new mails" color="inherit">
+					<Badge badgeContent={4} color="error">
+						<MailIcon />
+					</Badge>
+				</IconButton>
+				<p>Messages</p>
+			</MenuItem>
+			<MenuItem>
+				<IconButton
+					size="large"
+					aria-label="show 17 new notifications"
+					color="inherit"
+				>
+					<Badge badgeContent={17} color="error">
+						<NotificationsIcon />
+					</Badge>
+				</IconButton>
+				<p>Notifications</p>
+			</MenuItem>
+			<MenuItem onClick={handleProfileMenuOpen}>
+				<IconButton
+					size="large"
+					aria-label="account of current user"
+					aria-controls="primary-search-account-menu"
+					aria-haspopup="true"
+					color="inherit"
+				>
+					<AccountCircle />
+				</IconButton>
+				<p>Profile</p>
+			</MenuItem>
+		</Menu>
+	);
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
-            HENRY GAMES
-          </Typography>
+	React.useEffect(() => {
+		console.log(currentGames);
+		console.log(user.role);
+	}, [input]);
 
-          <Searchbar setInput={setInput} input={input} />
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography sx={{ display: { xs: 'none', md: 'flex' } }}>
+						<Link style={styles.link} to="/">
+							<Button
+								sx={{
+									my: 2,
+									color: 'white',
+									display: 'block',
+									fontSize: '20px',
+								}}
+							>
+								HENRY GAMES
+							</Button>
+						</Link>
+					</Typography>
 
-          <Box sx={{ flexGrow: 1 }} />
+					<Searchbar setInput={setInput} input={input} />
 
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Link style={styles.link} to='/'>
-              <Button sx={{ my: 2, color: "white", display: "block" }}>
-                Games
-              </Button>
-            </Link>
-            <Link style={styles.link}>
-              <Button sx={{ my: 2, color: "white", display: "block" }}>
-                About
-              </Button>
-            </Link>
-            <Link style={styles.link} to='/wishes'>
-              <Button
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                Wish list
-              </Button>
-            </Link>
-          </Box>
+					<Box sx={{ flexGrow: 1 }} />
 
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <CartWidget />
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Divider orientation='vertical' variant='middle' flexItem sx={{marginX: 3, color: 'white'}} /> 
-          <Box>
-            <SessionButton />
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
+					<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+						<Link style={styles.link} to="/">
+							<Button sx={{ my: 2, color: 'white', display: 'block' }}>
+								Games
+							</Button>
+						</Link>
+						<Link style={styles.link}>
+							<Button sx={{ my: 2, color: 'white', display: 'block' }}>
+								About
+							</Button>
+						</Link>
+						<Link style={styles.link} to="/wishes">
+							<Button sx={{ my: 2, color: 'white', display: 'block' }}>
+								Wish list
+							</Button>
+						</Link>
+					</Box>
 
-    </Box>
-  );
+					<Box
+						sx={{ display: { xs: 'none', md: 'flex', alignItems: 'center' } }}
+					>
+						<CartWidget />
+						{user.role === 'guest' ? null : (
+							<IconButton
+								size="large"
+								edge="end"
+								aria-label="account of current user"
+								aria-controls={menuId}
+								aria-haspopup="true"
+								onClick={handleProfileMenuOpen}
+								color="inherit"
+							>
+								<AccountCircle />
+							</IconButton>
+						)}
+					</Box>
+
+					<Divider
+						orientation="vertical"
+						variant="middle"
+						flexItem
+						sx={{ marginX: 3, color: 'white' }}
+					/>
+					<Box>
+						<SessionButton />
+					</Box>
+					<Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+						<IconButton
+							size="large"
+							aria-label="show more"
+							aria-controls={mobileMenuId}
+							aria-haspopup="true"
+							onClick={handleMobileMenuOpen}
+							color="inherit"
+						>
+							<MoreIcon />
+						</IconButton>
+					</Box>
+				</Toolbar>
+			</AppBar>
+			{renderMobileMenu}
+			{renderMenu}
+		</Box>
+	);
 };
 
 export default Navbar;
