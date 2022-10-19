7883e7be4b3f9f8dccd18d40de805dc4931d3c01
Cristianbenz <contactocbenitez@gmail.com>
2022-10-19 12:06:12 -0300

    feat(client/pages/Landing): add programmatically redirect if user is logged

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/credenciales";
 import { LandingContainer } from "../containers";
 import { LandingFormSection } from "../sections";
 
 export default function Landing() {
+  const navigate = useNavigate();
+
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+		return navigate('/home')
+    }
+  });
+
   return (
-		<LandingContainer>
-			<LandingFormSection />
-		</LandingContainer>
-	);
+    <LandingContainer>
+      <LandingFormSection />
+    </LandingContainer>
+  );
 }
