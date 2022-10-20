7a4aa7c264f9dc161a316aab0ae953abfbd6f255
lemarti <83716628+lemarti@users.noreply.github.com>
2022-10-20 09:27:17 -0300

    enum fixed

--- api/src/models/PurchaseOrder.js
+++ api/src/models/PurchaseOrder.js
@@ -1,42 +1,46 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define("purchaseOrder", {
+  sequelize.define("purchaseorder", {
     id: {
       type: DataTypes.INTEGER,
+      autoIncrement: true,
       allowNull: false,
       primaryKey: true,
       unique: true,
-      autoIncrement: true,
     },
-    //datos proveedor
+
     companyname: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       defaultValue: "GameScript",
     },
     companycuit: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       defaultValue: "20-45786529-3",
     },
     companyaddress: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       defaultValue: "calle siempre viva 123",
     },
     companyemail: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       defaultValue: "GameScript@gmail.com",
     },
     status: {
-      type: DataTypes.ENUM("received", "inprocess", "canceled", "finalized"),
+      type: DataTypes.STRING,
       allowNull: false,
+      defaultValue: "received",
+      validate: {
+        customValidator: (value) => {
+          const enums = ["received", "inprocess", "canceled", "finalized"];
+          if (!enums.includes(value)) {
+            throw new Error("not a valid option");
+          }
+        },
+      },
     },
     totalprice: {
       type: DataTypes.FLOAT,
