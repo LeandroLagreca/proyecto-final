f5b3c035de946542cfadb960bdd7c5af080b0c25
lemarti <83716628+lemarti@users.noreply.github.com>
2022-10-25 12:47:52 -0300

    ChangePurchaseOrder status id required by params status required by body

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -20,6 +20,7 @@ const {
   getAllOrders,
   createOrder,
   getUserOrders,
+  ChangeStatePurchaseOrder
 } = require("../controllers/Orders");
 
 const {
@@ -82,7 +83,7 @@ router.post("/login", PostLogin);
 router.get("/orders", getAllOrders);
 router.get("/orders/user", getUserOrders);
 router.post("/orders", createOrder);
-
+router.put("/orders/:id",ChangeStatePurchaseOrder)
 //Images
 router.post("/images/discounts", upLoadDicountsBanner);
 router.get("/images/discounts", getDiscountsBanner);
