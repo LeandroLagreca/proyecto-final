d168ba43793bb0921a2b4554896e1b779cb14fe7
DAVID HUARICANCHA <davidjss.dev@gmail.com>
2022-10-25 18:47:39 -0500

    maquetado Account

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,7 +1,77 @@
 import React from 'react';
+import {
+	Drawer,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	Divider,
+	Box,
+    Link
+} from '@mui/material';
+
+import { Person, LocalMall, NotificationsActive } from '@mui/icons-material';
 
 const Account = () => {
-	return <div>Account</div>;
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<Drawer
+				variant="permanent"
+				open={true}
+				anchor="left"
+				PaperProps={{ sx: { marginTop: '103px' } }}
+				sx={{
+					width: 240,
+					flexShrink: 0,
+					[`& .MuiDrawer-paper`]: {
+						width: 240,
+						boxSizing: 'border-box',
+					},
+				}}
+			>
+				<Box sx={{ overflow: 'auto' }}>
+					<List sx={{ paddingTop: 3 }}>
+						<Divider />
+						<ListItem>
+							<Link href="/" underline='none'>
+								<ListItemButton>
+									<ListItemIcon>
+										<Person />
+									</ListItemIcon>
+									<ListItemText primary={'My Profile'} />
+								</ListItemButton>
+							</Link>
+						</ListItem>
+						<Divider />
+						<ListItem>
+							<Link href="/" underline='none'>
+								<ListItemButton>
+									<ListItemIcon>
+										<LocalMall />
+									</ListItemIcon>
+									<ListItemText primary={'My Purchased Games'} />
+								</ListItemButton>
+							</Link>
+						</ListItem>
+						<Divider />
+						<ListItem>
+							<Link href="/" underline='none'>
+								<ListItemButton>
+									<ListItemIcon>
+										<NotificationsActive />
+									</ListItemIcon>
+									<ListItemText primary={'My Notification'} />
+								</ListItemButton>
+							</Link>
+						</ListItem>
+						<Divider />
+					</List>
+				</Box>
+			</Drawer>
+			<Box>Box</Box>
+		</Box>
+	);
 };
 
 export default Account;
