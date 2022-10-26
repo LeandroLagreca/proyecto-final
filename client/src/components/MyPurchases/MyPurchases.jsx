4f603b60cb186249bcfb594558662a99aa2ce640
DAVID HUARICANCHA <davidjss.dev@gmail.com>
2022-10-26 12:04:27 -0500

    Avanzando con PageAccount

---
+++ client/src/components/MyPurchases/MyPurchases.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyPurchases = () => {
+	return <div>MyPurchases</div>;
+};
+
+export default MyPurchases;
--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -8,71 +8,74 @@ import {
 	ListItemText,
 	Divider,
 	Box,
-    Link
+	Link,
 } from '@mui/material';
+import { AdminNavBar, Loader } from '../components';
+import { AdminContainer } from '../containers';
 
 import { Person, LocalMall, NotificationsActive } from '@mui/icons-material';
 
-//Maquetado by David
-
 const Account = () => {
 	return (
-		<Box sx={{ display: 'flex' }}>
-			<Drawer
-				variant="permanent"
-				open={true}
-				anchor="left"
-				PaperProps={{ sx: { marginTop: '103px' } }}
-				sx={{
-					width: 240,
-					flexShrink: 0,
-					[`& .MuiDrawer-paper`]: {
-						width: 240,
-						boxSizing: 'border-box',
-					},
-				}}
-			>
-				<Box sx={{ overflow: 'auto' }}>
-					<List sx={{ paddingTop: 3 }}>
-						<Divider />
-						<ListItem>
-							<Link href="/" underline='none'>
-								<ListItemButton>
-									<ListItemIcon>
-										<Person />
-									</ListItemIcon>
-									<ListItemText primary={'My Profile'} />
-								</ListItemButton>
-							</Link>
-						</ListItem>
-						<Divider />
-						<ListItem>
-							<Link href="/" underline='none'>
-								<ListItemButton>
-									<ListItemIcon>
-										<LocalMall />
-									</ListItemIcon>
-									<ListItemText primary={'My Purchased Games'} />
-								</ListItemButton>
-							</Link>
-						</ListItem>
-						<Divider />
-						<ListItem>
-							<Link href="/" underline='none'>
-								<ListItemButton>
-									<ListItemIcon>
-										<NotificationsActive />
-									</ListItemIcon>
-									<ListItemText primary={'My Notification'} />
-								</ListItemButton>
-							</Link>
-						</ListItem>
-						<Divider />
-					</List>
-				</Box>
-			</Drawer>
-			<Box>Box</Box>
-		</Box>
+		// <Box sx={{ display: 'flex' }}>
+		// 	<Drawer
+		// 		variant="permanent"
+		// 		open={true}
+		// 		anchor="left"
+		// 		PaperProps={{ sx: { marginTop: '103px' } }}
+		// 		sx={{
+		// 			width: 240,
+		// 			flexShrink: 0,
+		// 			[`& .MuiDrawer-paper`]: {
+		// 				width: 240,
+		// 				boxSizing: 'border-box',
+		// 			},
+		// 		}}
+		// 	>
+		// 		<Box sx={{ overflow: 'auto' }}>
+		// 			<List sx={{ paddingTop: 3 }}>
+		// 				<Divider />
+		// 				<ListItem>
+		// 					<Link href="/account/:id/" underline="none">
+		// 						<ListItemButton>
+		// 							<ListItemIcon>
+		// 								<Person />
+		// 							</ListItemIcon>
+		// 							<ListItemText primary={'My Profile'} />
+		// 						</ListItemButton>
+		// 					</Link>
+		// 				</ListItem>
+		// 				<Divider />
+		// 				<ListItem>
+		// 					<Link href="/" underline="none">
+		// 						<ListItemButton>
+		// 							<ListItemIcon>
+		// 								<LocalMall />
+		// 							</ListItemIcon>
+		// 							<ListItemText primary={'My Purchased Games'} />
+		// 						</ListItemButton>
+		// 					</Link>
+		// 				</ListItem>
+		// 				<Divider />
+		// 				<ListItem>
+		// 					<Link href="/" underline="none">
+		// 						<ListItemButton>
+		// 							<ListItemIcon>
+		// 								<NotificationsActive />
+		// 							</ListItemIcon>
+		// 							<ListItemText primary={'My Notification'} />
+		// 						</ListItemButton>
+		// 					</Link>
+		// 				</ListItem>
+		// 				<Divider />
+		// 			</List>
+		// 		</Box>
+		// 	</Drawer>
+		// 	<Box>Box</Box>
+		// </Box>
+		<AdminContainer>
+			{/* <AdminNavBar /> */}
+		</AdminContainer>
 	);
 };
 
