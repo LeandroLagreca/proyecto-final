fb1f177666d6381578027863df1193fab3234750
DAVID HUARICANCHA <davidjss.dev@gmail.com>
2022-10-28 12:17:28 -0500

    Se quito lo que no se necesita

--- client/src/components/MyProfile/MyProfile.jsx
+++ client/src/components/MyProfile/MyProfile.jsx
@@ -14,8 +14,6 @@ import {
 	IconButton,
 } from '@mui/material';
 
-import { styled } from '@mui/material/styles';
-
 import { SaveAs, PhotoCamera } from '@mui/icons-material';
 
 const MyProfile = () => {
