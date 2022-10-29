8e13fddae88cc279af9ebd42e530bdd554902c9d
Cristianbenz <contactocbenitez@gmail.com>
2022-10-29 16:57:35 -0300

    create question model

---
+++ api/src/models/Question.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+  sequelize.define('question', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    questions: {
+      type: DataTypes.JSON,
+      defaultValue: []
+    }
+  })
+};
