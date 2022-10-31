fd802617bbc2ef8fb06eddb5a085a23906f2caa3
Mario Villalba <villalbamario18@gmail.com>
2022-10-31 09:53:22 -0300

    prueba

--- client/src/components/Nosotros/Nosotros.jsx
+++ client/src/components/Nosotros/Nosotros.jsx
@@ -10,34 +10,42 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import {imgMario} from "../Nosotros/4x4mario.jpg"
+import imgMario from '../Nosotros/4x4mario.jpg';
 
 export default function MediaControlCard() {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  return (
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Mario Villalba
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Software Developer
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-          <LinkedInIcon sx={{ height: 38, width: 38 }}>
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </LinkedInIcon>
-        </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image={imgMario}
-        alt="Live from space album cover"
-      />
-    </Card>
-  );
+	return (
+		<Card sx={{ display: 'flex' }}>
+			<Box sx={{ display: 'flex', flexDirection: 'column' }}>
+				<CardContent sx={{ flex: '1 0 auto' }}>
+					<Typography component="div" variant="h5">
+						Mario Villalba
+					</Typography>
+					<Typography
+						variant="subtitle1"
+						color="text.secondary"
+						component="div"
+					>
+						Software Developer
+					</Typography>
+				</CardContent>
+				<Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+					<LinkedInIcon sx={{ height: 38, width: 38 }}>
+						{theme.direction === 'rtl' ? (
+							<SkipNextIcon />
+						) : (
+							<SkipPreviousIcon />
+						)}
+					</LinkedInIcon>
+				</Box>
+			</Box>
+			<CardMedia
+				component="img"
+				sx={{ width: 151 }}
+				image={imgMario}
+				alt="Live from space album cover"
+			/>
+		</Card>
+	);
 }
