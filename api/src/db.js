d41b221257f84ce5535ebb56c679b13dba3d96ed
leandro la greca <leandrolagreca@hotmail.com>
2022-11-01 12:55:53 -0300

    deploy app

--- api/src/db.js
+++ api/src/db.js
@@ -4,14 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const { generateKey } = require('crypto');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DATABASE_URL
 } = process.env;
-// cambiar name de db
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   // CONFIGURCION ADICIONAL
-  
+  dialectOptions:{
+    ssl:{
+      require:true,
+      rejectUnauthorized:false
+    }
+  }
 });
 const basename = path.basename(__filename);
 
