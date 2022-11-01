8dca3b5e926f81f94929ddc2dad481b6c2f143c0
Cristianbenz <contactocbenitez@gmail.com>
2022-10-31 22:36:49 -0300

    add new route for question in admin and update import on pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -9,3 +9,4 @@ export { default as AdminGames } from './AdminGames';
 export { default as Cart } from './cart/Cart';
 export { default as Discounts } from './Discounts';
 export { default as Account } from './Account';
+export { default as AdminQuestions } from './AdminQuestions';
