c9bdac9d6db9d5dbde1a4778f01b8a974e1ee09b
Cristianbenz <contactocbenitez@gmail.com>
2022-10-31 22:39:48 -0300

    merge conflicts into comments

--- client/src/sections/Comments.jsx
+++ client/src/sections/Comments.jsx
@@ -12,78 +12,90 @@ import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-import {Button,Typography,Container,Box,Grid,TextField,Paper,IconButton,Avatar,Rating,} from "@mui/material";
+import {
+  Button,
+  Typography,
+  Container,
+  Box,
+  Grid,
+  TextField,
+  Paper,
+  IconButton,
+  Avatar,
+  Rating,
+} from "@mui/material";
 import FloatingActionButtons from "../components/EditForm/BotonEditar";
 import Sidebar from "../components/Sidebar/Sidebar";
+import { useLocation } from "react-router-dom";
+import { AnswerModal, Answer } from "../components";
 import "./Comments.css";
 
 const parse = require("html-react-parser"); //funcion para parsear html
 const imgLink = "Url de imagen de usuario"; //aqui cuando agreguen la funcion de Imagen Profile
 
-export default function Comments({list, type}) {
+export default function Comments({ list, type }) {
+  const { pathname } = useLocation();
   const gameComment = list;
-    const user = useSelector((state) => state.user.status);
-    const dispatch = useDispatch();
-    var userId = "";
-    var userName = "";
-    if (auth.currentUser !== null) {
-      userId = auth.lastNotifiedUid;
-      userName = auth.currentUser.email;
-    }
+  const user = useSelector((state) => state.user.status);
+  const dispatch = useDispatch();
+  var userId = "";
+  var userName = "";
+  if (auth.currentUser !== null) {
+    userId = auth.lastNotifiedUid;
+    userName = auth.currentUser.email;
+  }
   useEffect(() => {}, [gameComment]);
 
   //Update de comentarios
-  async function handleSubmitUpdate(e)  {
+  async function handleSubmitUpdate(e) {
     if (!value.comment.text) {
       e.preventDefault();
       Swal.fire({
         toast: true,
-        icon: 'error',
-        title: 'You can not update a comment without text',
-        position: 'bottom-right',
+        icon: "error",
+        title: "You can not update a comment without text",
+        position: "bottom-right",
         showConfirmButton: false,
         timer: 3000,
-      })
-      return
+      });
+      return;
+    } else {
+      if (user === "guest") {
+        setIsLogued(false);
+        Swal.fire({
+          toast: true,
+          icon: "error",
+          title: "You cannot update a post if you are not registered",
+          position: "bottom-right",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+        return;
+      } else {
+        //value.comment.id = 4;
+        dispatch(updateComments(value));
+        setValue({
+          comment: {
+            id: "",
+            text: "",
+            userComment: "",
+            rating_like: 3,
+          },
+        });
+        setIsLogued(true);
+        Swal.fire({
+          toast: true,
+          icon: "success",
+          title: "Your review has been updated",
+          position: "bottom-right",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+        setUpdateButton(false);
+      }
     }
-    else {
-      if(user === "guest"){
-        setIsLogued(false)
-          Swal.fire({
-            toast: true,
-            icon: 'error',
-            title: 'You cannot update a post if you are not registered',
-            position: 'bottom-right',
-            showConfirmButton: false,
-            timer: 3000,
-          })
-          return
-        } else {
-          //value.comment.id = 4;
-          dispatch(updateComments(value));
-          setValue({
-            comment: {
-              id: "",
-              text: "",
-              userComment: "",
-              rating_like: 3,
-            },
-          })
-          setIsLogued(true)
-          Swal.fire({
-            toast: true,
-            icon: 'success',
-            title: 'Your review has been updated',
-            position: 'bottom-right',
-            showConfirmButton: false,
-            timer: 3000,
-          })
-          setUpdateButton(false);
-        }
-      }	
-    }
+  }
 
-  
   //Estado local de Form de Reseñas
   const [value, setValue] = React.useState({
     comment: {
@@ -92,8 +104,8 @@ export default function Comments({list, type}) {
       userComment: "",
       rating_like: 3,
     },
-  }); 
-  
+  });
+
   //Estado local para enviar Comment con negritas y demas
   const [already, setAlready] = React.useState({
     bold: false,
@@ -126,28 +138,31 @@ export default function Comments({list, type}) {
     });
   };
 
-    //{----------------------Icons de review--------------------}
+  //{----------------------Icons de review--------------------}
   //Handle para BOLD
   const handleBold = (e) => {
     if (already.bold === true) {
     } else {
       setValue({ ...value, comment: { text: `<b>${value.comment.text}</b>` } });
       setAlready({ ...already, bold: true });
-    }};
+    }
+  };
   //Handle para ITALIC
   const handleItalic = (event) => {
     if (already.italic === true) {
     } else {
       setValue({ ...value, comment: { text: `<i>${value.comment.text}</i>` } });
       setAlready({ ...already, italic: true });
-    }};
+    }
+  };
   //Handle para UNDERLINE
   const handleUnderline = (event) => {
     if (already.underline === true) {
     } else {
       setValue({ ...value, comment: { text: `<u>${value.comment.text}</u>` } });
       setAlready({ ...already, underline: true });
-    }};
+    }
+  };
   //Handle para LINK
   const handleLink = (event) => {
     if (already.link === true) {
@@ -157,7 +172,8 @@ export default function Comments({list, type}) {
         comment: { text: `<a href="#">${value.comment.text}` },
       });
       setAlready({ ...already, link: true });
-    }};
+    }
+  };
   //Handle para QUOTE
   const handleQuote = (event) => {
     if (already.quote === true) {
@@ -167,7 +183,8 @@ export default function Comments({list, type}) {
         comment: { text: `<blockquote>${value.comment.text}</blockquote>` },
       });
       setAlready({ ...already, quote: true });
-    }};
+    }
+  };
   //{------------------------------------------}
 
   const [UpdateButton, setUpdateButton] = React.useState(false);
@@ -179,37 +196,30 @@ export default function Comments({list, type}) {
       ...value,
     });
     setUpdateButton(true);
-  }
-
+  };
+  if (!list?.length) return <></>;
   return (
     <div style={{ padding: 0 }} className="Comments">
-      <h1>
-      {
-        type === "review" ?
-          'Reseñas' :
-          'Preguntas'
-      }
-      </h1>
-      {Array.isArray(gameComment)
-        ? gameComment.map((c) => {
-            return (
-              <Paper
-                elevation={4}
-                style={{ padding: "40px 20px", margin: "20px" }}
-                key={c.id}
-              >
-                {c.userComment == userName ? (                
-                <Grid item>
-                  <Typography
-                      style={{ textAlign: "right" }}
-                      component="legend">
-                      
-                      <Button variant="contained" name={c.id} onClick={handleUpdateButton}>
-                        Update
-                      </Button>
-                      
-                  </Typography>
-                  {  UpdateButton ?  (
+      <h1>{type === "review" ? "Reseñas" : "Preguntas"}</h1>
+      {gameComment.map((c) => {
+        return (
+          <Paper
+            elevation={4}
+            style={{ padding: "40px 20px", margin: "20px" }}
+            key={c.id}
+          >
+            {c.userComment === userName ? (
+              <Grid item>
+                <Typography style={{ textAlign: "right" }} component="legend">
+                  <Button
+                    variant="contained"
+                    name={c.id}
+                    onClick={handleUpdateButton}
+                  >
+                    Update
+                  </Button>
+                </Typography>
+                {UpdateButton ? (
                   <form onSubmit={handleSubmitUpdate} className="formComment">
                     <Box
                       width={580}
